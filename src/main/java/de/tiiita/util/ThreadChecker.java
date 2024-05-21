6de422c61ca2e89b5925052a1f4005e1e32cff5f
Tiiita <tiiitamail@gmail.com>
2024-05-21 14:24:31 +0200

    Add javadoc to ThreadChecker

--- src/main/java/de/tiiita/util/ThreadChecker.java
+++ src/main/java/de/tiiita/util/ThreadChecker.java
@@ -1,15 +1,44 @@
 package de.tiiita.util;
 
+/**
+ * Simple class that automatically reports when a method is called in the wrong thread.
+ */
 public class ThreadChecker {
 
+    /**
+     * Ad this to you methods body, and it will automatically throw an IllegalStateException if the method is called in the main thread.
+     */
     public static void asyncOnly() {
-        if (Thread.currentThread().getName().equalsIgnoreCase("main")) {
+        asyncOnly("main");
+    }
+
+    /**
+     * Ad this to you methods body, and it will automatically throw an IllegalStateException if the method is called in
+     * a different thread than the main thread.
+     */
+    public static void syncOnly() {
+        syncOnly("main");
+    }
+
+    /**
+     * Ad this to you methods body, and it will automatically throw an IllegalStateException if the method is called in the main thread.
+     *
+     * @param mainThread the name of the main thread. If the main thread is called "main" use {@link #asyncOnly()}
+     */
+    public static void asyncOnly(String mainThread) {
+        if (Thread.currentThread().getName().equalsIgnoreCase(mainThread)) {
             throw new IllegalStateException("Method is called sync but is marked as an async only method!");
         }
     }
 
-    public static void syncOnly() {
-        if (!Thread.currentThread().getName().equalsIgnoreCase("main")) {
+    /**
+     * Ad this to you methods body, and it will automatically throw an IllegalStateException if the method is called in
+     * a different thread than the main thread.
+     *
+     * @param mainThread the name of the main thread. If the main thread is called "main" use {@link #syncOnly()}
+     */
+    public static void syncOnly(String mainThread) {
+        if (!Thread.currentThread().getName().equalsIgnoreCase(mainThread)) {
             throw new IllegalStateException("Method is called async but is marked as a sync only method!");
         }
     }
