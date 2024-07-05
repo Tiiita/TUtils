41bdeb1e81b6b42879e8ffab0d705567947903e5
Tiiita <tiiitamail@gmail.com>
2024-07-05 14:15:55 +0200

    Fix color reset in logger Change to better looking logger colors

--- src/main/java/de/tiiita/logger/Logger.java
+++ src/main/java/de/tiiita/logger/Logger.java
@@ -5,25 +5,25 @@ import java.time.Instant;
 import java.util.Date;
 
 public abstract class Logger {
-    private static final String ANSI_YELLOW = "\u001B[33m";
-    private static final String ANSI_RED = "\u001B[31m";
-    private static final String ANSI_BLUE = "\u001B[34m";
+    private static final String ANSI_YELLOW = "\u001B[38;2;166;138;13m";
+    private static final String ANSI_RED = "\u001B[38;2;226;77;71m";
+    private static final String ANSI_BLUE = "\u001B[38;2;42;125;211m";
     private static final String ANSI_RESET = "\u001B[0m";
 
     public static void logInfo(String message) {
-        System.out.println(ANSI_RESET + getPrefix("inf") + " " + message);
+        System.out.println(ANSI_RESET + getPrefix("inf") + " " + message + ANSI_RESET);
     }
 
     public static void logWarning(String message) {
-        System.out.println(ANSI_YELLOW + getPrefix("war") + " " + message);
+        System.out.println(ANSI_YELLOW + getPrefix("war") + " " + message + ANSI_RESET);
 
     }
     public static void logError(String message) {
-        System.out.println(ANSI_RED + getPrefix("err") + " " + message);
+        System.out.println(ANSI_RED + getPrefix("err") + " " + message + ANSI_RESET);
     }
 
     public static void logDebug(String message) {
-        System.out.println(ANSI_BLUE + getPrefix("deb") + " " + message);
+        System.out.println(ANSI_BLUE + getPrefix("deb") + " " + message + ANSI_RESET);
     }
 
     //[22:01:59] [INFO]:
