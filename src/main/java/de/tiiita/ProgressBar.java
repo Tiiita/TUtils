c63ee3f2d5d09dd4c24d389c04d092d413c9a017
Tiiita <tiiitamail@gmail.com>
2024-07-18 02:23:27 +0200

    Recode ProgressBar

--- src/main/java/de/tiiita/ProgressBar.java
+++ src/main/java/de/tiiita/ProgressBar.java
@@ -1,18 +1,34 @@
 package de.tiiita;
 
 public class ProgressBar {
-    public String build(int progress) {
-        validateProgress(progress);
-        StringBuilder barBuilder = new StringBuilder();
+    private final int progressBarWidth;
 
-        barBuilder.append("(").append(progress).append("%) ");
-        barBuilder.append("|".repeat(Math.max(0, progress)));
+    public ProgressBar(int progressBarWidth) {
+        this.progressBarWidth = progressBarWidth;
+        validateBarSettings();
 
-        return barBuilder.toString();
     }
 
+    public String build(int progress) {
+        validateProgress(progress);
+
+        int numOfBars = (int) (progressBarWidth * ((double) progress / 100));
+
+        String progressBar = "(" + progress + "%) [" +
+                "|".repeat(Math.max(0, numOfBars)) +
+                " ".repeat(Math.max(0, progressBarWidth - numOfBars)) +
+                "]";
+
+        return progressBar;
+
+    }
+
+    private void validateBarSettings() {
+        if (progressBarWidth > 200 || progressBarWidth < 10)
+            throw new IllegalArgumentException("Bar width cannot be higher than 200 or lower than 10");
+    }
     private void validateProgress(int progress) {
-        if (progress > 100)
-            throw new IllegalArgumentException("Progress cannot be higher than 100%");
+        if (progress > 100 || progress < 0)
+            throw new IllegalArgumentException("Progress cannot be higher than 100 or lower than 0");
     }
 }
