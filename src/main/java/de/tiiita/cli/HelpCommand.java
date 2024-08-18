8794ab3863da16005e0e991d8c5a23066f43cfb4
tiiita <tiiitamail@gmail.com>
2024-08-18 22:35:58 +0200

    Add default help cmd

--- src/main/java/de/tiiita/cli/HelpCommand.java
+++ src/main/java/de/tiiita/cli/HelpCommand.java
@@ -2,6 +2,12 @@ package de.tiiita.cli;
 
 import java.util.concurrent.Callable;
 
+/**
+ * This is the default help command. You can just register this in your project
+ * to use it. If you want to implement the help command yourself, feel free to
+ * use this and modify it. But do not forget to register the right help command,
+ * you have to use the right import for the right help command!
+ */
 @Command(name = "help", description = "Show all commands and their description")
 public class HelpCommand implements Callable<Void> {
 
